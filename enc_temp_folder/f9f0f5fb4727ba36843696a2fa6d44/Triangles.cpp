44fe5419559a40868a6d9a6c031e54c241d8cbe0
Roitman24600 <daniel.roitman18@bathspa.ac.uk>
2020-01-09 15:56:53 +0000

    triangles

--- enc_temp_folder/f9f0f5fb4727ba36843696a2fa6d44/Triangles.cpp
+++ enc_temp_folder/f9f0f5fb4727ba36843696a2fa6d44/Triangles.cpp
@@ -1,20 +1,6 @@
 #include "Triangles.hpp"
-#include <random>
 
 void Triangles::setup(){
-
-	int c1 = rand();
-	int c2 = rand();
-	int c3 = rand();
-
-	int x1 = rand();
-	int x2 = rand();
-	int x3 = rand();
-
-	int y1 = rand();
-	int y2 = rand();
-	int y3 = rand();
-
 }
 
 void Triangles::update(){
@@ -23,14 +9,10 @@ void Triangles::update(){
 
 void Triangles::draw(){
     
-
 	ofSetColor(160,30,144);
 	ofDrawTriangle(200, 300, 100, 250, 300, 150);
 
 	ofSetColor(30, 97, 222);
 	ofDrawTriangle(868, 536, 455, 555, 639, 745);
-
-	//ofSetColor(self.c1, c2, c3);
-	//ofDrawTriangle(x1, y1, x2, y3, x3, y3);
 
 }
