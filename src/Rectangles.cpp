c68a7047671717a41782061329b559f3db5d7676
Tar2ed <thomas.reed17@bathspa.ac.uk>
2020-01-09 15:54:43 +0000

    finished rectangle

--- src/Rectangles.cpp
+++ src/Rectangles.cpp
@@ -1,13 +1,21 @@
 #include "Rectangles.hpp"
 
 void Rectangles::setup(){
-    
 }
 
 void Rectangles::update(){
-    
+	
 }
 
 void Rectangles::draw(){
-    
+	ofSetColor(19, 210, 50);
+	ofDrawRectangle(250, 600, 500, 200);
+	ofSetColor(152, 120, 23);
+	ofDrawRectangle(485, 50, 150, 200);
+	ofSetColor(25, 15, 60);
+	ofDrawRectangle(165, 300, 500, 200);
+	ofSetColor(10, 25, 39);
+	ofDrawRectangle(500, 268, 500, 200);
+	ofSetColor(69, 88, 64);
+	ofDrawRectangle(200, 788, 500, 200);
 }
